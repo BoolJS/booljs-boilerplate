7c46694aa173c83b4f820f523cd8fd75c70216b6
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-22 04:05:41 -0500

    Add dependencies to boilerplate configuration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var booljs = require('bool.js');
 
 // Here is where magic happens
-booljs('{{namespace}}')
+booljs('{{namespace}}'{{dependencies}})
     .setServerLoader('{{options.server_driver}}')
     .setDatabaseLoader('{{options.database_driver}}')
     .run();
