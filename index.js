4c5484f8ad385c49d8b7863437f2762bcb9d7d36
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-26 23:31:41 -0500

    Fix HTML-escaping syntax

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var booljs = require('bool.js');
 
 // Here is where magic happens
-booljs('{{namespace}}'{{dependencies}})
+booljs('{{namespace}}'{{{dependencies}}})
     .setServerLoader('{{options.server_driver}}')
     .setDatabaseLoader('{{options.database_driver}}')
     .run();
