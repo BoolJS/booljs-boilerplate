f9cd6b8e2b8b89d4d7cf7ef2a938e41010cf68d8
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-11-30 17:50:07 -0500

    Fix dependency route

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const Bool = require('booljs');
 
 // Here is where magic happens
-new Bool('com.example.api', [ 'node-uuid', require.resolve('..') ])
-    .run();
+new Bool('com.example.api', [
+    'node-uuid', 'booljs.passport', 'passport-http-bearer'
+]).run();
